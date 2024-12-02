ecd443b6e0f5e60519c7759c5b3c6fe0070e32ed
Lukas Gail <lukas.simon.gail@gmail.com>
2024-12-02 14:12:26 +0100

    added timing.

--- days/day02.py
+++ days/day02.py
@@ -1,62 +1,65 @@
-from utils.utils import load_data
+from utils.utils import load_data, time_decorator
 
-def level_tolerance_result(input_data, skip_level_tolerance: int = 0):
+@time_decorator
+def level_tolerance_result(input_data, skip_level: bool = False, use_brute_force: bool = False) -> int:
     save_reports_count = 0
     for line in input_data.split('\n'):
         if line:
             elements = [int(elem) for elem in line.split()]
-            rule_check_result = rule_check(elements, skip_level_tolerance)
+            rule_check_result = rule_check(elements, skip_level)
             if rule_check_result[0]:
                 save_reports_count += 1
 
-            elif skip_level_tolerance > 0:
-                if skip_index(elements, rule_check_result[1] - 1):
-                    save_reports_count += 1
+            elif skip_level:
+                if not use_brute_force:
+                    if skip_index(elements, rule_check_result[1] - 1):
+                        save_reports_count += 1
 
-                # bruteforce (works too)
-                # if brute_force(elements):
-                #     save_reports_count += 1
+                else:
+                    # bruteforce (works too)
+                    if brute_force(elements):
+                        save_reports_count += 1
 
     return save_reports_count
 
-# def brute_force(elements) -> bool:
-#     for i in range(len(elements)):
-#         elements_copy = elements.copy()
-#         elements_copy.pop(i)
-#         rule_check_result = rule_check(elements_copy, 0)
-#         if rule_check_result[0]:
-#             return True
-#     return False
+def brute_force(elements) -> bool:
+    for i in range(len(elements)):
+        elements_copy = elements.copy()
+        elements_copy.pop(i)
+        rule_check_result = rule_check(elements_copy, False)
+        if rule_check_result[0]:
+            return True
+    return False
 
 def skip_index(elements, index_to_skip) -> [bool, int]:
     if not -1 < index_to_skip < len(elements)-1:
         return False
     new_elements = elements.copy()
     new_elements.pop(index_to_skip)
-    if rule_check(new_elements, 0)[0]:
+    if rule_check(new_elements, False)[0]:
         return True
     return False
 
 
-def rule_check(elements, skip_level_tolerance) -> [bool, int]:
+def rule_check(elements, skip_level: bool) -> [bool, int]:
     prev_elem = elements[0]
     # Simple check for increasing or decreasing in first values, if not, check detailed.
     increasing = True if elements[0] < elements[1] < elements[2] else is_ascending(elements)
     valid_report = True
-    skipped_levels = 0
+    skipped_level = False
     first_skipped_index = -1
     for i in range(1, len(elements)):
         # rule 1: all increasing or all decreasing
         # rule 2: levels differ by at least one and at most three.
         if (not increasing is (prev_elem < elements[i]) or prev_elem == elements[i]
                 or abs(prev_elem - elements[i]) > 3):  # bools are singleton objects
-            skipped_levels += 1
             if first_skipped_index == -1:
                 first_skipped_index = i
 
-            if skipped_levels > skip_level_tolerance:
+            if skipped_level is skip_level:
                 valid_report = False
                 break
+            skipped_level = True # only once if skip_level is True
 
         else:
             prev_elem = elements[i]
@@ -75,6 +78,11 @@ def is_ascending(elements):
 
 if __name__ == '__main__':
     input_day02 = load_data('day02.txt')
-    print(level_tolerance_result(input_day02, 0))
-    print(level_tolerance_result(input_day02, 1))
+    #Task1
+    print(level_tolerance_result(input_day02, skip_level=False, use_brute_force=False))
+    print(level_tolerance_result(input_day02, skip_level=False, use_brute_force=True))
+
+    #Task2
+    print(level_tolerance_result(input_day02, skip_level=True, use_brute_force=False))
+    print(level_tolerance_result(input_day02, skip_level=True, use_brute_force=True))
 
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,22 @@
 import os
+import time
+from time import sleep
+
 
 def load_data(file_name):
     input_path = os.path.join(os.path.dirname(__file__), f'../inputs/{file_name}')
     with open(input_path, 'r') as file:
         data = file.read()
     return data
+
+def time_decorator(func):
+    def wrapper(*args, **kwargs):
+        # start_cpu = time.process_time()
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        # end_cpu = time.process_time()
+        end_time = time.perf_counter()
+        result_time = end_time - start_time
+        print(f"{func.__name__} needed {result_time:.6f} seconds.")
+        return result  # Func result
+    return wrapper
