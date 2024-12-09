80e372de4b130bfed8abc376b6cf5b812158da83
Lukas Gail <lukas.simon.gail@gmail.com>
2024-12-09 02:19:36 +0100

    day06

--- days/day06.py
+++ days/day06.py
@@ -27,26 +27,6 @@ def get_next_guard_delta(guard_delta: tuple[int, int]) -> tuple[int, int]:
     if guard_delta == (0, -1):  # left
         return -1, 0  # up
 
-# def mark_guard_path(matrix: list[list[str]], starting_pos: tuple[int, int]) -> int:
-#     """Return: possible loops"""
-#     guard_pos: tuple[int, int] = starting_pos
-#     guard_direction_delta = (-1, 0)  # up
-#     possible_loops = 0
-#
-#     while True:
-#         next_guard_pos = (guard_pos[0] + guard_direction_delta[0], guard_pos[1] + guard_direction_delta[1])
-#         if next_guard_pos[0] < 0 or next_guard_pos[0] >= len(matrix) or next_guard_pos[1] < 0 or next_guard_pos[1] >= len(matrix[0]):
-#             # Path end
-#             matrix[guard_pos[0]][guard_pos[1]] = 'X' # mark exiting point as visited.
-#             break
-#         if matrix[next_guard_pos[0]][next_guard_pos[1]] == '#':
-#             guard_direction_delta = get_next_guard_delta(guard_direction_delta)
-#         else:
-#             if is_loop_possible(matrix, next_guard_pos, guard_direction_delta):
-#                 possible_loops += 1
-#             matrix[guard_pos[0]][guard_pos[1]] = 'X'
-#             guard_pos = next_guard_pos
-#     return possible_loops
 
 def get_next_obstacle_cord_in_direction(matrix: list[list[str]], pos: tuple[int, int], direction_delta: tuple[int, int]):
     while True:
@@ -83,36 +63,66 @@ def get_guard_path_positions_with_direction(matrix: list[list[str]], starting_po
     return guard_positions_with_direction
 
 def is_loop_possible(matrix: list[list[str]], tmp_obstacle_pos: tuple[int, int], guard_pos: tuple[int, int], guard_dir_delta: tuple[int, int]) -> bool:
-    """Check if position in front of obstacle is reahed again."""
-    starting_pos = guard_pos
-    starting_dir = guard_dir_delta
+    """Check if position in front of obstacle is reached again."""
+    if tmp_obstacle_pos[0] < 0 or tmp_obstacle_pos[0] >= len(matrix) or tmp_obstacle_pos[1] < 0 or tmp_obstacle_pos[1] >= len(matrix[0]):
+        return False
     default_char = matrix[tmp_obstacle_pos[0]][tmp_obstacle_pos[1]]
     # set_tmp_obstacle
     matrix[tmp_obstacle_pos[0]][tmp_obstacle_pos[1]] = 'O'
+    tmp_obstacle_reached_deltas: list[tuple[int, int]] = []
+    obstacle_counter = 0
+    fourth_obstacle: tuple = (-1, -1)
+    fourth_obstacle_direction: tuple = (-1, -1)
 
     while True:
-        next_guard_pos = (guard_pos[0] + guard_dir_delta[0], guard_pos[1] + guard_dir_delta[1])
+        next_guard_pos: tuple[int, int] = (guard_pos[0] + guard_dir_delta[0], guard_pos[1] + guard_dir_delta[1])
         if next_guard_pos[0] < 0 or next_guard_pos[0] >= len(matrix) or next_guard_pos[1] < 0 or next_guard_pos[1] >= len(matrix[0]):
             # Path end
             matrix[tmp_obstacle_pos[0]][tmp_obstacle_pos[1]] = default_char
             return False
-        elif next_guard_pos[0] == starting_pos[0] and next_guard_pos[1] == starting_pos[1] and guard_dir_delta[0] == starting_dir[0] and guard_dir_delta[1] == starting_dir[1]:
+
+        elif matrix[next_guard_pos[0]][next_guard_pos[1]] == '#':
+            obstacle_counter = (obstacle_counter + 1)%4
+            if obstacle_counter == 0:
+                if fourth_obstacle == next_guard_pos and fourth_obstacle_direction == guard_dir_delta:
+                    # loop between 4 random obstacles
+                    matrix[tmp_obstacle_pos[0]][tmp_obstacle_pos[1]] = default_char
+                    return True
+                fourth_obstacle = next_guard_pos
+                fourth_obstacle_direction = guard_dir_delta
+
+            guard_dir_delta = get_next_guard_delta(guard_dir_delta)
+
+        elif matrix[next_guard_pos[0]][next_guard_pos[1]] == 'O':
+            if not guard_dir_delta in tmp_obstacle_reached_deltas:
+                tmp_obstacle_reached_deltas.append(guard_dir_delta)
+            else:
+                # already reached the obstacle in this direction
                 matrix[tmp_obstacle_pos[0]][tmp_obstacle_pos[1]] = default_char
                 return True
-        elif matrix[next_guard_pos[0]][next_guard_pos[1]] == '#' or matrix[next_guard_pos[0]][next_guard_pos[1]] == 'O':
+
             guard_dir_delta = get_next_guard_delta(guard_dir_delta)
+
         else:
             guard_pos = next_guard_pos
-        print(guard_pos)
 
 
-def get_possible_loops(matrix: list[list[str]], path_positions_and_directions: list[tuple[tuple[int, int], tuple[int, int]]]) -> int:
+
+def get_possible_loops(matrix: list[list[str]], path_positions_and_directions: list[tuple[tuple[int, int], tuple[int, int]]], guard_pos: tuple[int, int], guard_delta: tuple[int, int]) -> int:
     possible_loops = 0
-    for i, (pos, direct_delta) in enumerate(path_positions_and_directions):
-        tmp_obstacle_pos = (pos[0] + direct_delta[0], pos[1] + direct_delta[1])
-        if is_loop_possible(matrix, tmp_obstacle_pos, pos, direct_delta):
+    tested_obstacle_positions = set()
+
+    for i in range(1, len(path_positions_and_directions)-1):
+        tmp_obstacle_pos = path_positions_and_directions[i + 1][0]
+
+        if tmp_obstacle_pos in tested_obstacle_positions:
+            continue
+        else:
+            tested_obstacle_positions.add(tmp_obstacle_pos)
+
+        if is_loop_possible(matrix, tmp_obstacle_pos, guard_pos, guard_delta):
             possible_loops += 1
-            print(possible_loops)
+            print(f"({i}) {possible_loops}/{len(path_positions_and_directions)-2}")
 
     return possible_loops
 
@@ -120,14 +130,7 @@ def get_possible_loops(matrix: list[list[str]], path_positions_and_directions: l
 if __name__ == '__main__':
     input_day06 = load_data('day06.txt')
     input_matrix = transform_to_matrix(input_day06)
-    # res_possible_loops = mark_guard_path(input_matrix, get_guard_starting_position(input_matrix))
-    # 4977 task 1
-    # print(get_guard_path_length(input_matrix))
-    # print(res_possible_loops)
     positions_and_directions = get_guard_path_positions_with_direction(input_matrix, get_guard_starting_position(input_matrix), (-1, 0))
     print(get_guard_path_length(positions_and_directions))
-    print(get_possible_loops(input_matrix, positions_and_directions))
-
-
-
+    print(get_possible_loops(input_matrix, positions_and_directions, get_guard_starting_position(input_matrix), (-1, 0)))
 
